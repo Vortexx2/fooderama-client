73c89087f55c1067b0d0fe82dcd8b22fadd5458a
Vortexx2 <janmayshuklay@gmail.com>
2022-07-29 13:31:14 +0530

    Added 1 color theme settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,17 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      raisinblack: {
+        1: 'b6b9cd',
+        2: '#9296b5',
+        3: '#6d749c',
+        4: '#535879',
+        5: '#3a3d55',
+        6: '#212330',
+        DEFAULT: '#212330',
+      },
+    },
     extend: {},
   },
   plugins: [],
