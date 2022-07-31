a08e53d22217f4e648e3866de672a94a4d07088f
Vortexx2 <janmayshuklay@gmail.com>
2022-08-01 00:34:30 +0530

    Added dropdown functionality to hamburger menu

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,10 +4,17 @@ import CartIcon from './icons/CartIcon.vue'
 import Search from './utils/Search.vue'
 import SearchIcon from './icons/SearchIcon.vue'
 import HamburgerIcon from './icons/HamburgerIcon.vue'
+import CrossIcon from './icons/CrossIcon.vue'
 
 const colors = ref({
   cultured: '#edf4f2',
 })
+
+const dropDownOpen = ref(false)
+
+function toggleDropdown() {
+  dropDownOpen.value = !dropDownOpen.value
+}
 </script>
 
 <template>
@@ -46,11 +53,60 @@ const colors = ref({
     </span>
 
     <!-- Hamburger menu -->
-    <span class="ml-auto md:hidden">
+    <span class="ml-auto cursor-pointer md:hidden" @click="toggleDropdown">
       <HamburgerIcon
         :color="colors.cultured"
         class="w-[32px] h-[32px]"
       ></HamburgerIcon>
     </span>
+
+    <!-- Menu that opens with the hamburger -->
+    <transition name="dropdown">
+      <div
+        class="w-screen h-fit absolute top-0 right-0 bg-raisinb shadow-lg shadow-black z-50 transition"
+        v-if="dropDownOpen"
+      >
+        <!-- Cross Icon -->
+        <div
+          class="w-fit ml-auto m-5 p-2 cursor-pointer transition hover:bg-raisinb-5 hover:-translate-y-[2px] hover:shadow-lg hover:shadow-black"
+          @click="toggleDropdown"
+        >
+          <CrossIcon
+            :color="colors.cultured"
+            class="w-[25px] h-[25px]"
+          ></CrossIcon>
+        </div>
+        <div class="flex flex-col">
+          <div
+            class="text-2xl p-5 shadow-sm shadow-black transition cursor-pointer hover:bg-raisinb-5 hover:-translate-y-[2px] hover:shadow-lg hover:shadow-black"
+          >
+            Home
+          </div>
+          <div
+            class="text-2xl p-5 shadow-sm shadow-black transition cursor-pointer hover:bg-raisinb-5 hover:-translate-y-[2px] hover:shadow-lg hover:shadow-black"
+          >
+            Cart
+          </div>
+          <div
+            class="text-2xl p-5 shadow-sm shadow-black transition cursor-pointer hover:bg-raisinb-5 hover:-translate-y-[2px] hover:shadow-lg hover:shadow-black"
+          >
+            Login
+          </div>
+        </div>
+      </div>
+    </transition>
   </nav>
 </template>
+
+<style scoped>
+.dropdown-enter-active,
+.dropdown-leave-active {
+  transform-origin: top;
+  transition: transform 1s;
+}
+
+.dropdown-enter-from,
+.dropdown-leave-to {
+  transform: scaleY(0);
+}
+</style>
