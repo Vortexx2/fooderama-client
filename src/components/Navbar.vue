998a358c8777b117381947b18ad13f29a473dae4
Vortexx2 <janmayshuklay@gmail.com>
2022-08-01 18:32:06 +0530

    Added router links to all buttons

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,10 +1,12 @@
 <script setup>
 import { ref } from 'vue'
+import { RouterLink, useRouter } from 'vue-router'
+
 import CartIcon from './icons/CartIcon.vue'
-import Search from './utils/Search.vue'
-import SearchIcon from './icons/SearchIcon.vue'
 import HamburgerIcon from './icons/HamburgerIcon.vue'
 import CrossIcon from './icons/CrossIcon.vue'
+
+const router = useRouter()
 
 const colors = ref({
   cultured: '#edf4f2',
@@ -12,8 +14,20 @@ const colors = ref({
 
 const dropDownOpen = ref(false)
 
+/**
+ * Toggles the dropdown by toggling its display state.
+ */
 function toggleDropdown() {
   dropDownOpen.value = !dropDownOpen.value
+}
+
+/**
+ * Function triggered when a link is clicked in the dropdown navbar menu. Toggles dropdown to false and goes to the path provided.
+ * @param {string} path the path to go to when the dropdown link is clicked
+ */
+function clickedDropdownLink(path) {
+  dropDownOpen.value = false
+  router.push(path)
 }
 </script>
 
@@ -21,9 +35,9 @@ function toggleDropdown() {
   <nav
     class="w-full flex bg-raisinb text-white align-middle p-3 rounded-b-lg shadow-[7px_7px_5px_0px_rgba(0,0,0,0.3)] items-center"
   >
-    <span id="logo" class="mx-3 flex items-center">
+    <RouterLink to="/" id="logo" class="mx-3 flex items-center">
       <h3 class="text-2xl h-fit">Fooderama</h3>
-    </span>
+    </RouterLink>
 
     <!-- Search Component -->
     <!-- <span class="max-w-[175px] ml-auto sm:max-w-[200px] lg:-mr-52">
@@ -38,19 +52,21 @@ function toggleDropdown() {
     </span> -->
 
     <!-- Cart Button -->
-    <span
+    <RouterLink
+      to="/cart"
+      tag="span"
       class="ml-auto cursor-pointer hover:-translate-y-0.5 transition hidden md:inline"
     >
       <CartIcon
         :color="colors.cultured"
         class="w-[35px] h-[35px] -scale-x-100"
       />
-    </span>
+    </RouterLink>
 
     <!-- Login Button -->
-    <span class="align-middle ml-5 mr-2 hidden md:inline">
+    <RouterLink to="/login" class="align-middle ml-5 mr-2 hidden md:inline">
       <button class="btn-dark hover:-translate-y-0.5">Login</button>
-    </span>
+    </RouterLink>
 
     <!-- Hamburger menu -->
     <span class="ml-auto cursor-pointer md:hidden" @click="toggleDropdown">
@@ -79,16 +95,19 @@ function toggleDropdown() {
         <div class="flex flex-col">
           <div
             class="text-2xl p-5 shadow-sm shadow-black transition cursor-pointer hover:bg-raisinb-5 hover:-translate-y-[2px] hover:shadow-lg hover:shadow-black"
+            @click="clickedDropdownLink('/')"
           >
             Home
           </div>
           <div
             class="text-2xl p-5 shadow-sm shadow-black transition cursor-pointer hover:bg-raisinb-5 hover:-translate-y-[2px] hover:shadow-lg hover:shadow-black"
+            @click="clickedDropdownLink('/cart')"
           >
             Cart
           </div>
           <div
             class="text-2xl p-5 shadow-sm shadow-black transition cursor-pointer hover:bg-raisinb-5 hover:-translate-y-[2px] hover:shadow-lg hover:shadow-black"
+            @click="clickedDropdownLink('/login')"
           >
             Login
           </div>
@@ -102,7 +121,7 @@ function toggleDropdown() {
 .dropdown-enter-active,
 .dropdown-leave-active {
   transform-origin: top;
-  transition: transform 1s;
+  transition: transform 0.5s;
 }
 
 .dropdown-enter-from,
