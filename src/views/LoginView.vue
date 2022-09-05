0f4c45e73b954e59f3e66de22c141b198e481f51
Vortexx2 <janmayshuklay@gmail.com>
2022-09-06 00:43:18 +0530

    Added basic responsiveness to /login

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -4,7 +4,7 @@
   <main class="">
     <div
       id="container"
-      class="bg-raisinb w-5/12 mx-auto my-40 p-6 rounded-xl shadow-md shadow-black text-white">
+      class="bg-raisinb lg:w-5/12 w-3/4 mx-auto my-40 p-6 rounded-xl shadow-md shadow-black text-white">
       <!-- Login Heading -->
       <div class="text-center text-3xl mb-3">Login</div>
 
@@ -14,11 +14,13 @@
           <!-- Email input field -->
           <div id="email-pair" class="mb-6">
             <div class="text-left mb-2">
-              <label class="text-lg">Email:</label>
+              <label for="email" class="text-lg">Email:</label>
             </div>
             <div>
               <input
                 type="email"
+                required
+                id="email"
                 class="rounded-sm text-lg py-1 px-2 w-full text-black transition border-3 border-raisinb focus:outline-none focus:border-malachite-2 focus:border-3" />
             </div>
           </div>
@@ -26,17 +28,19 @@
           <!-- Password input field -->
           <div class="mb-6">
             <div class="text-left mb-2">
-              <label class="text-lg">Password:</label>
+              <label for="password" class="text-lg">Password:</label>
             </div>
             <div>
               <input
                 type="password"
+                required
+                id="password"
                 class="rounded-sm text-lg py-1 px-2 w-full text-black transition border-3 border-raisinb focus:outline-none focus:border-malachite-2 focus:border-3" />
             </div>
           </div>
 
           <div class="text-center">
-            <button class="btn-red px-3 py-2">Login</button>
+            <input type="submit" value="Login" class="btn-red px-3 py-2" />
           </div>
         </div>
       </form>
